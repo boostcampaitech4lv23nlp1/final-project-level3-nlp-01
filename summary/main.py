39d66490b7bf3340ddf943e81cd6575b229e3c81
eenzeenee <lililio0170@gmail.com>
2023-01-19 13:23:56 +0000

    chore: revise comment

--- summary/main.py
+++ summary/main.py
@@ -24,11 +24,11 @@ from summary import Summarizer
 from postprocess import PostProcessor
 
 
-def main_test(data_path, sum_model_path, sum_model):
+def main_test(data_path, sum_model_path, sum_model): # inference 시간 확인을 위한 임시 함수 -> 추후 삭제 예정
     start = time.time()
 
     # 1. STT output 파일 데이터셋 가져오기 (STT 후처리 완료된 파일)
-    ## 추후 STT output 형식 바뀔 수도 있으니까 따로 구분함
+    ## 추후 STT output 형식 바뀔 수도 있으니까 따로 분리함
     loader = DataLoader(data_path = data_path)
     stt_data = loader.load()
 
@@ -45,30 +45,30 @@ def main_test(data_path, sum_model_path, sum_model):
 
 
 
-    # 2. Summarization
+    # 3. Summarization
     summarizer = Summarizer(data = preprocessed,
                             model_path = sum_model_path,
                             max_input_length = 512,
                             max_target_length = 64,
                             model_ = sum_model) # kobart or t5
 
-    summarized_data = summarizer.summarize() # 문단 별 한 문장의 요약문 반환
+    summarized_data = summarizer.summarize() # 문단 별 한 문장의 요약문 list 반환
     summary_time = time.time()
-    # print(summarized_data) 
+
     print('# of summary :', len(summarized_data))
     print('time for summary :', summary_time - prep_time) # 14.77 초
 
-    # 3. Postprocessing
+    # 4. Postprocessing
     postprocessor = PostProcessor(data=summarized_data)
     postprocessed = postprocessor.postprocess()
     postprocess_time = time.time()
     print('# of postprocessed :', len(postprocessed))
-    print('time for postprocessing :', postprocess_time - summary_time)
+    print('time for postprocessing :', postprocess_time - summary_time) # 48.77 초 -> 후처리에 드는 시간 상당 ... 
     return postprocessed
 
 def main(data_path, sum_model_path, sum_model):
     # 1. STT output 파일 데이터셋 가져오기 (STT 후처리 완료된 파일)
-    ## 추후 STT output 형식 바뀔 수도 있으니까 따로 구분함
+    ## 추후 STT output 형식 바뀔 수도 있으니까 따로 분리함
     loader = DataLoader(data_path = data_path)
     stt_data = loader.load()
 
@@ -80,16 +80,16 @@ def main(data_path, sum_model_path, sum_model):
     preprocessed = preprocessor.preprocess()
 
 
-    # 2. Summarization
+    # 3. Summarization
     summarizer = Summarizer(data = preprocessed,
                             model_path = sum_model_path,
                             max_input_length = 512,
                             max_target_length = 64,
                             model_ = sum_model) # kobart or t5
 
-    summarized_data = summarizer.summarize() # 문단 별 한 문장의 요약문 반환
+    summarized_data = summarizer.summarize() # 문단 별 한 문장의 요약문 list 반환
 
-    # 3. Postprocessing
+    # 4. Postprocessing
     postprocessor = PostProcessor(data=summarized_data)
     postprocessed = postprocessor.postprocess()
     return postprocessed
