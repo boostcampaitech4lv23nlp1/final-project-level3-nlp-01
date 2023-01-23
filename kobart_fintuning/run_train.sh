4c3715f1856fb2127e0faf4c2762df2fa2536a5f
eenzeenee <lililio0170@gmail.com>
2023-01-23 12:16:38 +0000

    chore: remove summary training file

--- kobart_fintuning/run_train.sh
+++
@@ -1,6 +0,0 @@
-python train.py --gradient_clip_val 1.0 \
-                --max_epochs 10 \
-                --default_root_dir all_preprocessed_without_news \
-                --gpus 1 \
-                --batch_size 16 \
-                --num_workers 4
