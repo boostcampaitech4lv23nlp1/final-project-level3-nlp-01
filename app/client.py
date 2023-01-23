c151c5c9356185e3bea03735950e8c465fb9b731
eenzeenee <lililio0170@gmail.com>
2023-01-23 12:19:12 +0000

    feat: add server and client file

--- app/client.py
+++ app/client.py
@@ -6,30 +6,32 @@ import pandas as pd
 
 backend_address = "http://localhost:8001"
 
-def summarization(docs):
+def stt_sum(docs):
     data = {
         'docs': json.dumps(docs)
     }
 
     response = requests.get(
-        url = f"{backend_address}/summary",
+        url = f"{backend_address}/service",
         params = data
     )
 
     results = response.json()['output']
     json_data = json.loads(results)
-    st.dataframe(pd.DataFrame(json_data), 50000, 500)
+    st.write(json_data)
+    
     return
 
 
 def main():
-    st.title("Text Summarization")
-    uploaded_file = st.file_uploader("Choose a file!", type=["pickle"])
+    st.title("STT postprocessing + Summarization")
+    uploaded_file = st.file_uploader("Choose a file!", type=['csv'])
     if uploaded_file:
-        docs = pickle.load(uploaded_file)
+        docs = pd.read_csv(uploaded_file)
+        sentences = ' '.join(list(docs['output']))
         st.write(docs)
         st.subheader("Summary")
         with st.spinner('Wait for Summarization ...'):
-            summarization(docs)
+            stt_sum(sentences)
 
 main()
--- app/server.py
+++ app/server.py
@@ -1,23 +1,50 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from .summary.main import main_test
+from .stt_postprocessing.test import main_inference
 import json
+
+import pickle
 
 app = FastAPI()
 
-# @app.get("/")
-# def hello_world():
-#     return {"hello": "world"}
-
-@app.get("/summary")
-def summary(docs):
+def STT_postprocessing(docs):
     input = json.loads(docs)
-    output = main_test(input)
+    output = main_inference(model_path='/opt/ml/project_models/stt/postprocessing_gpt',max_len=64, data = input)
+    print('finish postprocessing')
+    return output
+
+
+def summary(data):
+    output = main_test(data = data,
+                        sum_model_path='/opt/ml/project_models/summarization/kobart_all_preprocessed_without_news',
+                        sum_model= 'kobart')
+    print('finish summarization')
     return JSONResponse(
         status_code = 200,
         content = {
         "output": json.dumps(output)
         }
     )
+# TODO: add keyword_extraction function - async ?
+# async def keyword_extraction(docs):
+#     input = json.loads(docs)
+#     output = main_test(input)
+#     return JSONResponse(
+#         status_code = 200,
+#         content = {
+#         "output": json.dumps(output)
+#         }
+#     )
 
+# TODO: add question_generation function
+
+@app.get("/service")
+def main(docs):
+    stt_post_processed = STT_postprocessing(docs)
+    with open('/opt/ml/stt_postprocessed.pickle', 'wb') as f:
+        pickle.dump(stt_post_processed, f)
+    summarized = summary(stt_post_processed)
+    # extracted_keyword = keyword_extraction(stt_post_processed)
+    return summarized
 
