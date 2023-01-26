d665d685f1213ee245d366deaf0b8805dc90d8e2
eenzeenee <lililio0170@gmail.com>
2023-01-26 06:40:13 +0000

    feat: revise stt postprocessing function

--- app/stt_postprocessing/dataloader.py
+++ app/stt_postprocessing/dataloader.py
@@ -1,86 +1,15 @@
-import numpy as np
-import torch
-from torch.utils.data import Dataset
-import warnings
-warnings.filterwarnings("ignore")
+import pandas as pd
 
 
-OUTPUT_TKN = "<usr>"
-RESULT_TKN = "<sys>" 
-BOS = '</s>'
-EOS = '</s>'
-MASK = '<unused0>'
-SENT = '<unused1>'
-PAD = '<pad>'
+class DataLoader:
+    def __init__(self, data_path):
+        self.data_path = data_path
 
-class STTDataset(Dataset):
-    def __init__(self, datasets, tokenizer, max_len=64):  # 데이터셋의 전처리를 해주는 부분
-        self._data = datasets
-        self.max_len = max_len
-        self.output_token = OUTPUT_TKN
-        self.result_token = RESULT_TKN
-        self.sent_token = SENT
-        self.eos = EOS
-        self.mask = MASK
-        self.tokenizer = tokenizer
+    def load(self):
+        data = pd.read_csv(self.data_path)
+        return data
 
-    def __len__(self):  # STTdata 의 길이를 리턴한다.
-        return len(self._data)
-
-    def __getitem__(self, idx):  # 로드한 챗봇 데이터를 차례차례 DataLoader로 넘겨주는 메서드
-        df = self._data.iloc[idx]
-        query = df["output"]  # 질문을 가져온다.
-
-        answer = df["label"]  # 답변을 가져온다.
-
-        q_tokenized = self.tokenizer.tokenize(self.output_token + str(query) + self.sent_token)
-        q_len = len(q_tokenized)
-
-        a_tokenized = self.tokenizer.tokenize(self.result_token + str(answer) + self.eos)
-        a_len = len(a_tokenized)
-
-        #질문의 길이가 최대길이보다 크면
-        if q_len > self.max_len:
-            a_len = self.max_len - q_len        #답변의 길이를 최대길이 - 질문길이
-            if a_len <= 0:       #질문의 길이가 너무 길어 질문만으로 최대 길이를 초과 한다면
-                q_tokenized = q_tokenized[-(int(self.max_len / 2)) :]   #질문길이를 최대길이의 반으로 
-                q_len = len(q_tokenized)
-                a_len = self.max_len - q_len              #답변의 길이를 최대길이 - 질문길이
-            a_tokenized = a_tokenized[:a_len]
-            a_len = len(a_tokenized)
-
-        #질문의 길이 + 답변의 길이가 최대길이보다 크면
-        if q_len + a_len > self.max_len:
-            a_len = self.max_len - q_len        #답변의 길이를 최대길이 - 질문길이
-            if a_len <= 0:       #질문의 길이가 너무 길어 질문만으로 최대 길이를 초과 한다면
-                q_tokenized = q_tokenized[-(int(self.max_len / 2)) :]   #질문길이를 최대길이의 반으로 
-                q_len = len(q_tokenized)
-                a_len = self.max_len - q_len              #답변의 길이를 최대길이 - 질문길이
-            a_tokenized = a_tokenized[:a_len]
-            a_len = len(a_tokenized)
-
-        # 답변 labels = [mask, mask, ...., mask, ..., <bos>,..답변.. <eos>, <pad>....]
-        labels = [self.mask,] * q_len + a_tokenized[1:]
-
-        # mask = 질문길이 0 + 답변길이 1 + 나머지 0
-        mask = [0] * q_len + [1] * a_len + [0] * (self.max_len - q_len - a_len)
-        # 답변 labels을 index 로 만든다.
-        labels_ids = self.tokenizer.convert_tokens_to_ids(labels)
-        # 최대길이만큼 PADDING
-        while len(labels_ids) < self.max_len:
-            labels_ids += [self.tokenizer.pad_token_id]
-
-        # 질문 + 답변을 index 로 만든다.
-        token_ids = self.tokenizer.convert_tokens_to_ids(q_tokenized + a_tokenized)
-        # 최대길이만큼 PADDING
-        while len(token_ids) < self.max_len:
-            token_ids += [self.tokenizer.pad_token_id]
-
-        #질문+답변, 마스크, 답변
-        return (token_ids, np.array(mask), labels_ids)
-    
-def collate_batch(batch):
-    data = [item[0] for item in batch]
-    mask = [item[1] for item in batch]
-    label = [item[2] for item in batch]
-    return torch.LongTensor(data), torch.LongTensor(mask), torch.LongTensor(label)
+if __name__ == '__main__':
+    data_path = '/opt/ml/espnet-asr/STT_postprocessing/history_dataset.csv'
+    loader = DataLoader(data_path=data_path)
+    loader.load()
--- app/stt_postprocessing/main.py
+++ app/stt_postprocessing/main.py
@@ -1,72 +1,79 @@
-import time
-import warnings
-warnings.filterwarnings("ignore")
-
-import numpy as np
-import kss
+import pandas as pd
 import torch
+import time
 from transformers import PreTrainedTokenizerFast
 from transformers import GPT2LMHeadModel
+import warnings
+from multiprocessing import Process
+from .inference import inference
+from .split_data import split_file
+warnings.filterwarnings("ignore")
 
 
-
-def postprocess(model_path, max_len, data):
-
-    OUTPUT_TKN = "<usr>"
-    RESULT_TKN = "<sys>"
+def postprocess(model_path, df):
+    
     BOS = '</s>'
     EOS = '</s>'
     MASK = '<unused0>'
-    SENT = '<unused1>'
     PAD = '<pad>'
 
-    start_time = time.time()
+    num_process = 8
+    
+    print(f'num process : {num_process}')
+    
+    scps = split_file(df, split = 1)
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
     tokenizer = PreTrainedTokenizerFast.from_pretrained(model_path,
                 bos_token=BOS, eos_token=EOS, unk_token='<unk>',
                 pad_token=PAD, mask_token=MASK) 
-    model = GPT2LMHeadModel.from_pretrained(model_path)
-
-    sent = '0'
+    model = GPT2LMHeadModel.from_pretrained(model_path).to(device)
+    max_len = 64
+    processes = []
     sentences = []
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
-    answer=""
+    outputs = []
+    if len(scps) > 1:
+        model.share_memory()
 
-    with torch.no_grad():
-        setences = list(kss.split_sentences(data))
-        for i in range(0,len(setences)-2,3):
-            query = ' '.join(setences[i:i+3])
-            print(f'query : {query}')
-            answer = ""
-            
-            if query is not np.nan:
-                while True:
-                    input_ids = torch.LongTensor(tokenizer.encode(OUTPUT_TKN + query + SENT + sent + RESULT_TKN + answer)).unsqueeze(dim=0)
-                    input_ids = input_ids.to(device)
-                    pred = model(input_ids)
-                    pred = pred.logits
-                    if pred.shape[1]>max_len:
-                        print("error!!")
-                        break
-                    gen = tokenizer.convert_ids_to_tokens(torch.argmax(pred, dim=-1).squeeze().cpu().numpy().tolist())[-1]
-                    if gen == EOS:
-                        break
-                    answer += gen.replace("▁", " ")
-                print(f"answer : {answer.strip()}")
-                sentences.append(answer.strip())
-            else:
-                sentences.append(query)
-                
-    print(f'sec : {time.time()-start_time}')
+        for i, scp in enumerate(scps):
+            output_dir= f'./output/inference_{i}'
+            args = [model, tokenizer, scp, max_len, output_dir]
+ 
+            process = Process(target=inference,args=args)
+            process.start()
+            processes.append(process)
+            time.sleep(0.1)
+
+        for process in processes:
+            process.join()
+        processes.clear()
+    else:
+        output_dir = './output/inference_0'
+        num_process = 1
+        args = [model, tokenizer, scps[0], max_len, output_dir]
+        inference(*args)
+    
+    for i in range(num_process):
+        df = pd.read_csv(f'./output/inference_{i}.csv')
+        for _, item in df.iterrows():
+            sentences.append(item['result'])
+    #         outputs.append(item['output'])
+    # data = pd.DataFrame({
+    #     'output' : outputs,
+    #     'result' : sentences
+    # })
+    # data.to_csv('./inference.csv', index=False)
+    
     return sentences
 
 
 if __name__ == '__main__':
-    import pandas as pd
-    data = pd.read_csv('/opt/ml/[심화별개념5]_2_1구석기_신석기시대_2강선사시대_dataset.csv')
-
-    result = stt_postprocess(model_path = '/opt/ml/project_models/stt/postprocessing_gpt/', max_len = 64,
-                       data = data)
-
-    print(result)
-                       
+    from dataloader import DataLoader
+    start_time = time.time()
+    data_path = '/opt/ml/espnet-asr/STT_postprocessing/history_dataset.csv'
+    loader = DataLoader(data_path=data_path)
+    df = loader.load()
+    torch.multiprocessing.set_start_method('spawn',force=True)
+    results = postprocess(model_path = '/opt/ml/espnet-asr/final/GPT_2', df = df)
+    print(results)
+    print(f'sec : {time.time()-start_time}')
