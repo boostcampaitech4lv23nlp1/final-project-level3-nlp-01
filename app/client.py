5207d383b9bd4c2c2973f1d0baa1d9249dbbabf4
eenzeenee <lililio0170@gmail.com>
2023-01-26 02:26:34 +0000

    chore: for sharing

--- app/client.py
+++ app/client.py
@@ -1,37 +1,77 @@
 import requests
-import pickle
 import json
 import streamlit as st
 import pandas as pd
 
 backend_address = "http://localhost:8001"
 
-def stt_sum(docs):
-    data = {
-        'docs': json.dumps(docs)
-    }
+# def stt(docs):
+#     data = {
+#         'docs':json.dumps(docs)
+#     }
+#     response = requests.post(
+#         url = f'{backend_address}/saveWavFile/',
+#         params = data
+#     )
+#     response = requests.get(
+#         url=f'{backend_address}/sppechToText/',
+#     )
+#     return response
+
+# def stt_postprocess(docs):
+#     data = {
+#         'docs':json.dumps(docs)
+#     }
+#     response = requests.get(
+#         url = f'{backend_address}/sppechToText/',
+#         params = data
+#     )
+#     return response
+
+def stt(docs):
+    data = {"file": docs}
+    headers = {"Content-type": "application/json"}
+
+    response = requests.post(
+        url = f"{backend_address}/saveWavFile/",
+        data = json.dumps(data),
+        headers = headers
+    )
 
     response = requests.get(
-        url = f"{backend_address}/service",
-        params = data
+        url = f"{backend_address}/speechToText/"
     )
+
+    response = requests.get(
+        url = f"{backend_address}/sttPostProcessing/",
+        data = response
+    )
+
+    response = requests.get(
+        url = f"{backend_address}/segmentation/"
+    )
+
+    response = requests.get(
+        url = f"{backend_address}/summarization/",
+        data = response
+    )
+    print('>>>>>>>>>>>> finish summarization')
 
     results = response.json()['output']
     json_data = json.loads(results)
     st.write(json_data)
     
-    return
 
+    return response
 
 def main():
-    st.title("STT postprocessing + Summarization")
-    uploaded_file = st.file_uploader("Choose a file!", type=['csv'])
-    if uploaded_file:
-        docs = pd.read_csv(uploaded_file)
-        sentences = ' '.join(list(docs['output']))
-        st.write(docs)
-        st.subheader("Summary")
-        with st.spinner('Wait for Summarization ...'):
-            stt_sum(sentences)
+    st.title("STT -> postprocessing -> preprocessing -> Summary")
 
-main()
+    uploaded_file = '/opt/ml/stt/backend/stt_example_1.wav'
+    if uploaded_file:
+        with st.spinner('wait for summarization'):
+            result = stt(uploaded_file)
+    st.write(result)
+    
+if __name__ == '__main__':        
+    main()
--- app/server.py
+++ app/server.py
@@ -1,16 +1,26 @@
 # TODO: validation check
-# TODO: stt function
+
 # TODO: keyword extraction function
 # TODO: question generation function
 # TODO: async test
 
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from .summary.main import main
-from .stt_postprocessing.test import main_inference
 import json
-
+import shutil
 import pickle
+import torch
+import uvicorn
+from pydantic import BaseModel
+
+from typing import Optional
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+from starlette.middleware.cors import CORSMiddleware
+
+
+from summary.main import summ_preprocess, summarize
+from stt_postprocessing.main import stt_postprocess
+from STT.setup import stt_setup
+
 
 app = FastAPI()
 
@@ -23,24 +33,124 @@ app.add_middleware(
     allow_headers=['*']
 )
 
-def STT_postprocessing(docs):
-    input = json.loads(docs)
-    output = main_inference(model_path='/opt/ml/project_models/stt/postprocessing_gpt',max_len=64, data = input)
-    print('finish postprocessing')
-    return output
+class FileName(BaseModel):
+    file:str
+
+class STTOutput(BaseModel):
+    stt_output:str
+
+class STTPostprocessed(BaseModel):
+    stt_postprocessed:str
+
+# STT + postprocess + segmentation
+@app.post('speechToText', description = 'stt inference')
+def stt_inference(file: FileName):
+    if file is None:
+        return {'output':None}
+    else:
+         # with open(str(file.file), 'rb') as f:
+        #     shutil.copyfileobj(file.file, f) ## commit 할때는 주석 풀기
+        app.wav_filename = file.file
+        
+        torch.multiprocessing.set_start_method('spawn')     # multiprocess mode
+        app.stt_output = stt_setup(
+            make_dataset=False, 
+            inference_wav_file=app.wav_filename
+        )
+        postprocessed = stt_postprocess(model_path='/opt/ml/project_models/stt/postprocessing_gpt',max_len=64, data = app.stt_output)
 
 
-def summary(data):
-    output = main(data = data,
-                        sum_model_path='/opt/ml/project_models/summarization/kobart_all_preprocessed_without_news',
-                        sum_model= 'kobart')
-    print('finish summarization')
-    return JSONResponse(
-        status_code = 200,
-        content = {
-        "output": json.dumps(output)
-        }
-    )
+# input WAV file to save
+@app.post('/saveWavFile/', description='save wav file')
+# def save_wav_file(file: UploadFile=File(...)): ## commit 할때는 이 형식으로 데이터 불러오기
+def save_wav_file(file: FileName):
+    if file is None:
+        return {'output': None}
+    else:
+        # with open(str(file.file), 'rb') as f:
+        #     shutil.copyfileobj(file.file, f) ## commit 할때는 주석 풀기
+        app.wav_filename = file.file
+        return JSONResponse(
+            status_code = 200,
+            content = {
+            "output": json.dumps(file.file)
+            })
+
+# STT inference
+@app.get('/speechToText/', description='stt inference')
+def stt_inference(file: FileName):
+    try:
+        filename = file
+        torch.multiprocessing.set_start_method('spawn')     # multiprocess mode
+        output = stt_setup(
+            make_dataset=False, 
+            inference_wav_file=filename
+        )
+        return JSONResponse(
+            status_code = 200,
+            content = {
+            "output": json.dumps(output)
+            })
+    except AttributeError as e:
+        return {'error':'start STT inference error'}
+
+## AttributeError: 'FastAPI' object has no attribute 'stt_output' -> error 발생
+## client에서 requests 보낼때의 문제 ~ json 구성 바꾸기
+
+# STT postprocess
+@app.get('/sttPostProcessing/', description='stt postprocessing')
+def stt_postprocess(stt_output: STTOutput):
+    try:
+        input = stt_output
+        output = stt_postprocess(model_path='/opt/ml/project_models/stt/postprocessing_gpt',max_len=64, data = input)
+        return JSONResponse(
+            status_code = 200,
+            content = {
+            "output": json.dumps(output)
+            }
+        )
+    except AttributeError as e:
+        return {'error':'start STT inference error'}
+
+# Make phrase
+@app.get('/segmentation/', description='make phrase')
+def preprocess(stt_postprocessed: STTPostprocessed):
+    try:
+        input = stt_postprocessed
+        output = summ_preprocess(input)
+        return JSONResponse(
+            status_code = 200,
+            content = {
+            "output": json.dumps(output)
+            }
+        )
+    except AttributeError as e:
+        return {'error':'start preprocessing error'}
+
+
+#######################################
+
+# Summarization
+@app.get('/summarization/', description='start summarization')
+def summary():
+    try:
+        input = app.preprocessed
+        output = summarize(data = input,
+                            sum_model_path='/opt/ml/project_models/summarization/kobart_all_preprocessed_without_news',
+                            sum_model= 'kobart')
+        print('finish summarization')
+        return JSONResponse(
+            status_code = 200,
+            content = {
+            "output": json.dumps(output)
+            }
+        )
+    except AttributeError as e:
+        return {'error':'start summarization error'}
+
+########################################
+
+
 # TODO: add keyword_extraction function - async ?
 # async def keyword_extraction(docs):
 #     input = json.loads(docs)
@@ -54,12 +164,15 @@ def summary(data):
 
 # TODO: add question_generation function
 
-@app.get("/service")
-def main(docs):
-    stt_post_processed = STT_postprocessing(docs)
-    with open('/opt/ml/stt_postprocessed.pickle', 'wb') as f:
-        pickle.dump(stt_post_processed, f)
-    summarized = summary(stt_post_processed)
-    # extracted_keyword = keyword_extraction(stt_post_processed)
-    return summarized
+# @app.get("/service")
+# def main(docs):
+#     stt_post_processed = STT_postprocessing(docs)
+#     with open('/opt/ml/stt_postprocessed.pickle', 'wb') as f:
+#         pickle.dump(stt_post_processed, f)
+#     summarized = summary(stt_post_processed)
+#     # extracted_keyword = keyword_extraction(stt_post_processed)
+#     return summarized
 
+if __name__ == "__main__":
+    
+    uvicorn.run(app, host="127.0.0.1", port=8001)
