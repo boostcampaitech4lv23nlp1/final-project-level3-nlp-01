8c24fb52f25ee13a7673cd0f952f62d475b872ad
eenzeenee <lililio0170@gmail.com>
2023-01-26 04:56:21 +0000

    fix: fix bugs

--- app/STT/data/utils/custom_split.py
+++ app/STT/data/utils/custom_split.py
@@ -11,9 +11,12 @@ def custom_split(dfs: pd.DataFrame) -> pd.DataFrame:
     for c in text:
         if end_flag is False:
             if c == ' ' and len(output) > max_length:
-                if len(outputs[-1]) < max_length:
-                    outputs[-1] += output
-                else:
+                try:
+                    if len(outputs[-1]) < max_length:
+                        outputs[-1] += output
+                    else:
+                        outputs.append(output + ' ')
+                except IndexError as indexError:
                     outputs.append(output + ' ')
                 output = ""
                 continue
