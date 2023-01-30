3924cf86ba2e0c65c86f14bfec5d5e62b72ed7e8
eenzeenee <lililio0170@gmail.com>
2023-01-30 07:55:20 +0000

    fix: fix max retries error

--- app/client.py
+++ app/client.py
@@ -11,31 +11,42 @@ def stt(docs:str): # for streamlit test -> str (filepath)
     data = {"file": docs}
     headers = {"Content-type": "application/json"}
 
-    response = requests.post(
-        url = f"{backend_address}/saveWavFile/",
-        data = json.dumps(data),
-        headers = headers
-    )
+    page = ''
+    while page == '':
+        try:
+            response = requests.post(
+                url = f"{backend_address}/saveWavFile/",
+                data = json.dumps(data),
+                headers = headers
+            )
 
-    response = requests.get(
-        url = f"{backend_address}/speechToText/"
-    )
+            response = requests.get(
+                url = f"{backend_address}/speechToText/"
+            )
 
-    response = requests.get(
-        url = f"{backend_address}/sttPostProcessing/",
-    )
+            response = requests.get(
+                url = f"{backend_address}/sttPostProcessing/",
+            )
 
-    response = requests.get(
-        url = f"{backend_address}/segmentation/"
-    )
+            response = requests.get(
+                url = f"{backend_address}/segmentation/"
+            )
 
-    results = response.json()['output']
-    json_data = json.loads(results)
-    return json_data
+            results = response.json()['output']
+            json_data = json.loads(results)
+            return json_data
 
-def summary(docs:list):
-    data = {'segments':json.dumps(docs)}
-    # headers = {"Content-type": "application/json"}
+        except:
+            print("Connection refused by the server..")
+            print("Let me sleep for 5 seconds")
+            print("ZZzzzz...")
+            time.sleep(5)
+            print("Was a nice sleep, now let me continue...")
+            continue
+
+
+def summary(segments):
+    data = {'segments':json.dumps(segments)}
     response = requests.post(
         url = f"{backend_address}/summarization/",
         params = data,
@@ -44,6 +55,7 @@ def summary(docs:list):
     results = response.json()['output']
     json_data = json.loads(results)
     return json_data
+
 
 def keyword(seg_docs, summary_docs):
     data = {
@@ -71,74 +83,44 @@ def keyword(seg_docs, summary_docs):
             continue
 
 
-def keyword(seg_docs, summary_docs):
-    data = {
-        'seg_docs': json.dumps(seg_docs),
-        'summary_docs': json.dumps(summary_docs)
-    }
-
-    response = requests.get(
-        url = f"{backend_address}/keyword",
-        params = data,
-        verify = False
-    )
-
-    results = response.json()['output']
-    json_data = json.loads(results)
-    return pd.DataFrame(json_data)
-
-
 def qg(keywords):
-    keywords = keywords.to_json()
     data = {
-        'keywords': keywords
+        'keywords': json.dumps(keywords)
     }
-
-    response = requests.get(
+    response = requests.post(
         url = f"{backend_address}/qg",
         params = data,
         verify=False
     )
 
     results = response.json()['output']
-    json_data = json.loads(results)
-    return json_data
+    qg_result = json.loads(results)
+    return qg_result
 
 def main():
     st.title("BACKEND test")
 
-    uploaded_file = '/opt/ml/level3_productserving-level3-nlp-01/history-03.wav'
+    uploaded_file = '/opt/ml/history.wav'
     if uploaded_file:
         with st.spinner('wait for stt'):
-            stt_inferenced = stt(uploaded_file)
+            segments = stt(uploaded_file)
     st.subheader("STT segments")
-    st.write(stt_inferenced)
-    st.write(type(stt_inferenced))
-    st.write(stt_inferenced[0])
-    print(type(stt_inferenced))
-
-    summarized = summary(stt_inferenced)
+    st.write(segments)
+    with st.spinner('wait for summarization'):
+        summarized = summary(segments)
     st.subheader("Summarization Result")
     st.write(summarized)
 
     #### keyword test
     st.subheader("Keyword Extraction Result")
-
-    with open('seg_0125.pickle', 'rb') as f:
-        seg_docs = pickle.load(f)
-    
-    with open("seg_0125.pickle", "rb" ) as f:
-        summary_docs = pickle.load(f)
-
-    keywords = keyword(seg_docs, summary_docs)
-    st.dataframe(keywords, 50000, 500)
+    with st.spinner('wait for keyword extraction'):
+        keywords = keyword(segments, summarized)
+    st.write(keywords)
 
     ##### qg test
     st.subheader("Question Generation, Result")
-    # with open('0126_keywords.pickle', 'rb') as f:
-    #     keywords = pickle.load(f)
-
-    qg_result = qg(keywords)
+    with st.spinner('wait for question generation'):
+        qg_result = qg(keywords)
     st.write(qg_result)
 
     
