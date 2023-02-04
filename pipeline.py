3f38e272379961ba2b949057f0e030f8f37b978b
wooy0ng <yygg9800@naver.com>
2023-02-04 19:07:36 +0000

    etc: ignore userwarning

--- pipeline.py
+++ pipeline.py
@@ -1,11 +1,12 @@
 import torch
 from STT.setup import stt_setup
+import warnings
 
+warnings.filterwarnings('ignore', category=UserWarning)
 
 if __name__ == '__main__':
     torch.multiprocessing.set_start_method('spawn')     # multiprocess mode
     output = stt_setup(
         make_dataset=False, 
-        inference_wav_file='./[심화별개념5]_ 2-1 구석기_신석기 시대_2강 선사시대.wav'
+        inference_wav_file='./[심화별개념5]_2-1구석기_신석기시대_2강선사시대.wav'
     )
-    
